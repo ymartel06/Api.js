e04ac1a3181a4fa4871a60e61ef0ef51106b7937
Yohann MARTEL <yohann.martel@gmail.com>
2015-10-22 16:04:25 -0400

    Fix article route

--- app/routes/articles.js
+++ app/routes/articles.js
@@ -1,13 +1,15 @@
 ﻿'use strict';
 
-var articles = require('../controllers/articles');
+var validator = require('validator'),
+	response = require('../helpers/response'),
+	articles = require('../controllers/articles');
 
 module.exports = function(app) {
 	app.route('/articles').get(articles.findAll).post(articles.addArticle);
 	app.route('/articles/:articleId').get(articles.findById).put(articles.updateArticle).delete(articles.deleteArticle);
 	
 	//validation of articleId
-	app.param('articleId', function (req, res, next, id) {
+	app.param('articleId', function (req, res, next) {
 		if (!validator.isNull(req.params.articleId))
 		{
 			next();	
